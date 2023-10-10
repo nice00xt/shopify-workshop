472a5f3bddb00ed20b0000b1dd2d126aa0dada05
Juan Mejia <juan00xt@gmail.com>
2023-10-09 23:42:30 -0500

    Replace liquid sintax, add Lit component for image gallery

--- components/src/component-product.js
+++ components/src/component-product.js
@@ -2,16 +2,47 @@ import { LitElement, html } from 'lit';
 
 class ProductComponent extends LitElement {
   static properties = {
+    currentVariant: {
+      type: Object
+    },
     product: {
       type: Object
     }
   }
 
   render() {
+    console.log(this.product, 'this.currentVariant')
     return html`
-      <div>
-        <h1>Product component !..ss</h1>
-        <span>${this.product.title}</span>
+      <div class="flex w-full">
+        <div class="grid flex-grow card place-items-center overflow-hidden">
+
+          <div class="carousel w-[400px]">ss
+            ${this.product.media.map(({ src, alt }, idx) => {
+              return html`
+              <div id="item${idx}" class="carousel-item w-full">
+                <div class="card w-96 bg-base-100 shadow-xl">
+                  <figure>
+                    <img src='${src}' alt='${alt}'/>
+                  </figure>
+                </div>
+              </div>
+            `;
+            })}
+          </div>
+          <div class="flex justify-center w-full py-2 gap-2 mt-5">
+            ${this.product.media.map(({ src, alt }, idx) => {
+              return html`
+              <div class="avatar">
+                <a href="#item${idx}" class="w-20 rounded">
+                  <img src="${src}" alt="${alt}">
+                </a>
+              </div>
+            `;
+            })}
+          </div>
+
+
+        </div>
       </div>
     `;
   }
